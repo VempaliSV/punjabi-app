39226d10cebdc3ca28471a0d3e712188b35b9399
VempaliSV <47567238+VempaliSV@users.noreply.github.com>
2026-07-09 20:34:13 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,10 @@
+const CACHE_NAME = 'punjabi-v1';
+const ASSETS = ['./', './index.html', './words.js', './manifest.json'];
+
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('punjabi-store').then((cache) => cache.addAll([
-      './',
-      './index.html',
-      './manifest.json',
-    ])),
-  );
+    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request)),
-  );
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
